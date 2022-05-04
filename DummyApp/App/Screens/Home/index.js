b04021ec0499a18c0b1302e471da73c741ca519c
vikram <vikram.singh@digitalcrafters.tech>
2022-05-05 01:12:21 +0530

    committed when open project in android studio

--- DummyApp/App/Screens/Home/index.js
+++ DummyApp/App/Screens/Home/index.js
@@ -1,26 +1,30 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState, useRef} from 'react';
-import {Text, View, TouchableOpacity, Button, Modal} from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  Modal,
+  TextInput,
+} from 'react-native';
 import styles from './styles';
-let interval = null;
-const Home = ({navigation}) => {
-  const initalState = 0;
-  const [count, setCount] = useState(initalState);
-  const counterRef = useRef(initalState);
-  const [activeTab, setActiveTab] = useState(2);
-  const [signalColorOne, setSignalColorOne] = useState('green');
-  const [signalColorTwo, setSignalColorTwo] = useState('red');
-  const [signalColorThree, setSignalColorThree] = useState('red');
-  const [signalColorFour, setSignalColorFour] = useState('red');
-  const [countOne, setCountOne] = useState(10);
-  const [countTwo, setCountTwo] = useState(10);
-  const [countThree, setCountThree] = useState(10);
-  const [countFour, setCountFour] = useState(10);
-  const [showModal, setShowModal] = useState(false);
+import SegmentedControl from '../Components';
 
-  useEffect(() => {
-    counterRef.current = count;
-  });
+var timer = null;
+const Home = ({navigation}) => {
+  const [activeTab, setActiveTab] = useState(2);
+  const [showModal, setShowModal] = useState(false);
+  const [tabIndex, setTabIndex] = useState(0);
+  const [duration, setDuration] = useState(10);
+  const [flowDuration, setFlowDuration] = useState(10);
+  const [flowType, setFlowType] = useState(0);
+  const [manualFlowType, setManualFlowType] = useState(false);
+
+  const handleTabsChange = index => {
+    console.log(index);
+    setTabIndex(index);
+  };
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -31,85 +35,100 @@ const Home = ({navigation}) => {
   }, [navigation]);
 
   const showSettings = () => {
-    setShowModal(!showModal);
+    setShowModal(true);
+  };
+
+  const [timeLeft, setTimeLeft] = useState(flowDuration);
+
+  const startTimer = () => {
+    timer = setTimeout(() => {
+      if (timeLeft <= 0) {
+        clearTimeout(timer);
+        start();
+        return false;
+      }
+      setTimeLeft(timeLeft - 1);
+      console.log('vikram', timeLeft - 1);
+    }, 1000);
   };
 
   useEffect(() => {
-    //For Resume the interval call
-    const unsubscribe = navigation.addListener('focus', () => {
-      interval = setInterval(() => {
-        setCount(counterRef.current + 1);
-      }, 1000);
-      return () => clearInterval(interval);
-    });
-    return unsubscribe;
-  }, [navigation]);
+    startTimer();
+    return () => clearTimeout(timer);
+  });
 
-  useEffect(() => {
-    if (count % 10 == 0) {
-      if (activeTab == 1) {
-        setActiveTab(4);
-        setSignalColorFour('green');
-        setSignalColorOne('red');
-        setSignalColorTwo('red');
-        setSignalColorThree('red');
-      } else if (activeTab == 2) {
-        setActiveTab(1);
-        setSignalColorFour('red');
-        setSignalColorOne('green');
-        setSignalColorTwo('red');
-        setSignalColorThree('red');
-      } else if (activeTab == 3) {
-        setActiveTab(2);
-        setSignalColorFour('red');
-        setSignalColorOne('red');
-        setSignalColorTwo('green');
-        setSignalColorThree('red');
-      } else if (activeTab == 4) {
-        setActiveTab(3);
-        setSignalColorFour('red');
-        setSignalColorOne('red');
-        setSignalColorTwo('red');
-        setSignalColorThree('green');
+  const start = () => {
+    setTimeLeft(flowDuration);
+    clearTimeout(timer);
+    startTimer();
+    if (manualFlowType == true) {
+      setManualFlowType(false);
+      alert('ddd');
+    } else {
+      if (flowType == 0) {
+        if (activeTab == 1) {
+          setActiveTab(4);
+        } else if (activeTab == 2) {
+          setActiveTab(1);
+        } else if (activeTab == 3) {
+          setActiveTab(2);
+        } else if (activeTab == 4) {
+          setActiveTab(3);
+        }
+      } else if (flowType == 1) {
+        if (activeTab == 1) {
+          setActiveTab(2);
+        } else if (activeTab == 2) {
+          setActiveTab(3);
+        } else if (activeTab == 3) {
+          setActiveTab(4);
+        } else if (activeTab == 4) {
+          setActiveTab(1);
+        }
+      } else if (flowType == 2) {
+        if (activeTab == 1) {
+          setActiveTab(3);
+        } else if (activeTab == 2) {
+          setActiveTab(4);
+        } else if (activeTab == 3) {
+          setActiveTab(2);
+        } else if (activeTab == 4) {
+          setActiveTab(1);
+        }
       }
     }
-    if (count > 86400000) {
-      // Refresh the clock after 24 hr.
-      setCount(0);
-    }
-    console.log('main effect', activeTab);
-  }, [navigation, count]);
+  };
 
   const changeManually = signalNumber => {
-    if (1 == signalNumber) {
-      setActiveTab(signalNumber);
-      setSignalColorFour('red');
-      setSignalColorOne('green');
-      setSignalColorTwo('red');
-      setSignalColorThree('red');
-    } else if (2 == signalNumber) {
-      setActiveTab(signalNumber);
-      setSignalColorFour('red');
-      setSignalColorOne('red');
-      setSignalColorTwo('green');
-      setSignalColorThree('red');
-    } else if (3 == signalNumber) {
-      setActiveTab(signalNumber);
-      setSignalColorFour('red');
-      setSignalColorOne('red');
-      setSignalColorTwo('red');
-      setSignalColorThree('green');
-    } else if (4 == signalNumber) {
-      setActiveTab(signalNumber);
-      setSignalColorFour('green');
-      setSignalColorOne('red');
-      setSignalColorTwo('red');
-      setSignalColorThree('red');
+    setActiveTab(signalNumber, setManualFlowType(false), start());
+  };
+  const onPressModolClose = () => {
+    setShowModal(false);
+  };
+  const onPressUpdate = () => {
+    if (duration && duration < 301 && duration >= 10) {
+      console.log(duration);
+      setFlowType(tabIndex);
+      setFlowDuration(duration);
+      setTimeLeft(duration);
+      onPressModolClose();
+      alert('Updated');
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('Duration should be 10 to 300');
     }
-    // setCount(0);
   };
   return (
     <View style={styles.container}>
+      <Text
+        style={styles.hintText}>{`Signal Duration is:${flowDuration}`}</Text>
+      <Text style={styles.hintText}>{`Signal Rotation is:${
+        flowType == 0
+          ? 'Clockwise'
+          : flowType == 1
+          ? 'Anti-Clockwise'
+          : 'Up to Down - Left to Right'
+      }`}</Text>
       <View style={styles.uperView}>
         <TouchableOpacity
           style={styles.button}
@@ -121,9 +140,14 @@ const Home = ({navigation}) => {
             AMB
           </Text>
         </TouchableOpacity>
-        <View style={[styles.secondView, {backgroundColor: signalColorOne}]} />
+        <View
+          style={[
+            styles.secondView,
+            {backgroundColor: activeTab == 1 ? 'green' : 'red'},
+          ]}
+        />
         <Text style={{alignItems: 'center', marginVertical: 8}}>
-          {countOne}
+          {activeTab == 1 ? timeLeft : 'Wait'}
         </Text>
       </View>
       <View style={styles.horiView}>
@@ -138,13 +162,25 @@ const Home = ({navigation}) => {
               AMB
             </Text>
           </TouchableOpacity>
-          <View style={[styles.otherView, {backgroundColor: signalColorTwo}]} />
-          <Text style={styles.textMiddle}>{countTwo}</Text>
+          <View
+            style={[
+              styles.otherView,
+              {backgroundColor: activeTab == 2 ? 'green' : 'red'},
+            ]}
+          />
+          <Text style={styles.textMiddle}>
+            {activeTab == 2 ? timeLeft : 'Wait'}
+          </Text>
         </View>
         <View style={styles.text}>
-          <Text style={styles.textMiddle}>{countThree}</Text>
+          <Text style={styles.textMiddle}>
+            {activeTab == 4 ? timeLeft : 'Wait'}
+          </Text>
           <View
-            style={[styles.otherView, {backgroundColor: signalColorFour}]}
+            style={[
+              styles.otherView,
+              {backgroundColor: activeTab == 4 ? 'green' : 'red'},
+            ]}
           />
           <TouchableOpacity
             style={styles.otherButton}
@@ -160,10 +196,13 @@ const Home = ({navigation}) => {
       </View>
       <View style={styles.uperView}>
         <Text style={{alignItems: 'center', marginVertical: 8}}>
-          {countFour}
+          {activeTab == 3 ? timeLeft : 'Wait'}
         </Text>
         <View
-          style={[styles.secondView, {backgroundColor: signalColorThree}]}
+          style={[
+            styles.secondView,
+            {backgroundColor: activeTab == 3 ? 'green' : 'red'},
+          ]}
         />
         <TouchableOpacity
           style={styles.button}
@@ -176,6 +215,54 @@ const Home = ({navigation}) => {
           </Text>
         </TouchableOpacity>
       </View>
+      <Modal
+        //backdropOpacity={0.3}
+        animationType="fade"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={() => {}}>
+        <View style={styles.modalView}>
+          <View style={styles.subModalView}>
+            <Text style={styles.hintText}>Select the Signal rotation</Text>
+            <Text style={styles.hintText}>
+              {'1.Clockwise 2.Anti-Clockwise 3.Up to Down - Left to Right'}
+            </Text>
+            <SegmentedControl
+              tabs={['Clock', 'Anti-C', 'U-D , L-R']}
+              currentIndex={tabIndex}
+              onChange={handleTabsChange}
+              segmentedControlBackgroundColor="#86c4fd"
+              activeSegmentBackgroundColor="#0482f7"
+              activeTextColor="white"
+              textColor="black"
+              paddingVertical={18}
+            />
+            <Text style={styles.hintText}>Enter Signal Duration</Text>
+            <TextInput
+              maxLength={3}
+              keyboardType="number-pad"
+              underlineColorAndroid="transparent"
+              value={String(duration)}
+              placeholder="Enter The Time Duration"
+              returnKeyType={'done'}
+              onChangeText={text => setDuration(text)}
+              style={styles.InputTextStyle}
+            />
+            <TouchableOpacity
+              style={styles.loginScreenButton}
+              onPress={onPressUpdate}
+              underlayColor="#fff">
+              <Text style={styles.loginText}>UPDATE</Text>
+            </TouchableOpacity>
+            <Button
+              title={'Cancel'}
+              onPress={onPressModolClose}
+              textStyle={{marginHorizontal: 30}}
+              color="red"
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
